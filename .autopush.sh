0547d3c17281cd0b89b3ebd7520e7dad461be47e
paperpass1024 <paper_pass1024@163.com>
2024-08-02 16:41:44 +0800

    20240802

--- .autopush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(readlink -f "$( dirname "${BASH_SOURCE[0]}" )" )"
-SCRIPT_DIR_NAME=$(basename "$SCRIPT_DIR" )
-cd "$SCRIPT_DIR" || return
-git add .
-git commit -m $today
-git push "$SCRIPT_DIR_NAME" master 
